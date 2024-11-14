195b051429a8ab471529bd2318c1c34cbfe058ae
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-11-14 19:07:17 +0600

    Navbar complete.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -11,7 +12,9 @@ const HomeLayout = () => {
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav></nav>
+            <nav className='w-11/12 mx-auto py-4'>
+                <Navbar></Navbar>
+            </nav>
             <main></main>
             
         </div>
