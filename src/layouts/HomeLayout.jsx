2560240d65705abb6c6321392a7f7ddedbf02590
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-11-14 19:34:38 +0600

    Left Navbar added done.

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import Header from '../components/Header';
-import LatestNews from '../components/LatestNews';
-import Navbar from '../components/Navbar';
+import React from "react";
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+import LeftNavbar from "../components/layout-component/LeftNavbar";
 
 const HomeLayout = () => {
-    return (
-        <div className='font-Poppins'>
-            <header>
-                <Header></Header>
-                <section className='w-11/12 mx-auto'>
-                    <LatestNews></LatestNews>
-                </section>
-            </header>
-            <nav className='w-11/12 mx-auto py-4'>
-                <Navbar></Navbar>
-            </nav>
-            <main></main>
-            
-        </div>
-    );
+  return (
+    <div className="font-Poppins">
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
+      <nav className="w-11/12 mx-auto py-4">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-4">
+      <aside className="left col-span-3">
+        <LeftNavbar></LeftNavbar>
+      </aside>
+      <section className="col-span-6"></section>
+      <aside className="right col-span-3"></aside>
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
